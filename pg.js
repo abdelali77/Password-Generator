1a4f037158acf56d543c6025529b7c107ad65d5f
Abdelali <abde69436@gmail.com>
2024-06-06 19:27:28 +0100

    main

--- pg.js
+++ pg.js
@@ -8,8 +8,8 @@ import savePassword from './utils/savePassword.js';
 program.version('1.0.0').description('Password Generator');
 
 program
-  .option('-l, --length <number>', 'length of password', '6')
-  .option('-s, --save', 'save password as password.txt')
+	.option('-l, --length <number>', 'length of password', '8')
+	.option('-s, --save', 'save password as password.txt')
 	.option('-nn, --no-numbers', 'remove numbers from password')
 	.option('-ns, --no-symbols', 'remove symbols from password')
 	.option('-nl, --no-letters', 'remove letters from password')
